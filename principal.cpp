399f118a8b420391df8c78eb8ecfb6c00f607438
Alejandro Aceves <aceves.dimax@gmail.com>
2014-09-30 00:29:21 -0500

    Asi solo detecta el paso del mouse

--- principal.cpp
+++ principal.cpp
@@ -5,14 +5,14 @@
 #include <string>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 602;
+const int SCREEN_HEIGHT = 602;
 
 //Variables de jugadores
-char* STR_TITULO = "JUEGO del GATO";
+char STR_TITULO[20] = "JUEGO del GATO";
 int turno = 0;
-char* player1 = "Jugador 1";
-char* player2 = "Jugador 2";
+char player1[20] = "Jugador 1";
+char player2[20] = "Jugador 2";
 
 char tablero[3][3];
 const int espacios = 3;
@@ -20,8 +20,8 @@ char tipo = '0';
 
 //Botones
 //Button constants
-const int BUTTON_WIDTH = 260;
-const int BUTTON_HEIGHT = 200;
+const int BUTTON_WIDTH = 199;
+const int BUTTON_HEIGHT = 199;
 const int TOTAL_BUTTONS = 9;
 
 enum LButtonSprite
@@ -104,6 +104,85 @@ class LButton
         LButtonSprite mCurrentSprite;
 };
 
+
+
+//Starts up SDL and creates window
+bool init();
+//Loads media
+bool loadMedia();
+//Frees media and shuts down SDL
+void close();
+//Loads individual image as texture
+SDL_Texture* loadTexture( std::string path );
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+//Current displayed texture
+SDL_Texture* gTexture = NULL;
+
+//Mouse button sprites
+SDL_Rect gSpriteClips[ BUTTON_SPRITE_TOTAL ];
+LTexture gButtonSpriteSheetTexture;
+
+//Buttons objects
+LButton gButtons[ TOTAL_BUTTONS ];
+
+LTexture::LTexture()
+{
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+	//Deallocate
+	free();
+}
+
+bool LTexture::loadFromFile( std::string path )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
 #ifdef _SDL_TTF_H
 bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
 {
@@ -139,6 +218,152 @@ bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColo
     return mTexture != NULL;
 }
 #endif
+
+void LTexture::free()
+{
+	//Free texture if it exists
+	if( mTexture != NULL )
+	{
+		SDL_DestroyTexture( mTexture );
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+	//Modulate texture rgb
+	SDL_SetTextureColorMod( mTexture, red, green, blue );
+}
+
+void LTexture::setBlendMode( SDL_BlendMode blending )
+{
+	//Set blending function
+	SDL_SetTextureBlendMode( mTexture, blending );
+}
+
+void LTexture::setAlpha( Uint8 alpha )
+{
+	//Modulate texture alpha
+	SDL_SetTextureAlphaMod( mTexture, alpha );
+}
+
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+	//Set clip rendering dimensions
+	if( clip != NULL )
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	//Render to screen
+	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
+}
+
+int LTexture::getWidth()
+{
+	return mWidth;
+}
+
+int LTexture::getHeight()
+{
+	return mHeight;
+}
+
+//Inicializar el boton en una posicion del sprite
+LButton::LButton()
+{
+	mPosition.x = 0;
+	mPosition.y = 0;
+
+	mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+}
+
+
+
+void LButton::setPosition( int x, int y )
+{
+	mPosition.x = x;
+	mPosition.y = y;
+}
+
+
+void LButton::handleEvent( SDL_Event* e )
+{
+	//If mouse event happened
+	if( e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP )
+	{
+		//Get mouse position
+		int x, y;
+		SDL_GetMouseState( &x, &y );
+
+		//Check if mouse is in button
+		bool inside = true;
+
+		//Mouse is left of the button
+		if( x < mPosition.x )
+		{
+			inside = false;
+		}
+		//Mouse is right of the button
+		else if( x > mPosition.x + BUTTON_WIDTH )
+		{
+			inside = false;
+		}
+		//Mouse above the button
+		else if( y < mPosition.y )
+		{
+			inside = false;
+		}
+		//Mouse below the button
+		else if( y > mPosition.y + BUTTON_HEIGHT )
+		{
+			inside = false;
+		}
+
+		//Mouse is outside button
+		if( !inside )
+		{
+			mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+		}
+		//Mouse is inside button
+		else
+		{
+			//Set mouse over sprite
+			switch( e->type )
+			{
+                //TODO:  REvisar que player sigue, para asignar la imagen adecuada.
+                //TODO: Verificar si hay ganador
+                //TODO: Incrementar turno
+
+
+				case SDL_MOUSEMOTION:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
+				break;
+
+				case SDL_MOUSEBUTTONDOWN:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_DOWN;
+				break;
+
+				case SDL_MOUSEBUTTONUP:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
+				break;
+			}
+		}
+	}
+}
+
+//Renderear justo cuando el mouse esta en una posicion del  boton.
+void LButton::render()
+{
+	//Show current button sprite
+	gButtonSpriteSheetTexture.render( mPosition.x, mPosition.y, &gSpriteClips[ mCurrentSprite ] );
+}
 
 //--------------------- JUEGO ------------
 
@@ -185,20 +410,6 @@ void muestraGanador()
 //-------------- GRAFICOS
 
 
-//Starts up SDL and creates window
-bool init();
-//Loads media
-bool loadMedia();
-//Frees media and shuts down SDL
-void close();
-//Loads individual image as texture
-SDL_Texture* loadTexture( std::string path );
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-//Current displayed texture
-SDL_Texture* gTexture = NULL;
 
 
 bool init()
@@ -266,6 +477,40 @@ bool loadMedia()
 	{
 		printf( "Failed to load texture image!\n" );
 		success = false;
+	}
+
+
+
+	//Load sprites
+	if( !gButtonSpriteSheetTexture.loadFromFile( "images/button.png" ) )
+	{
+		printf( "Failed to load button sprite texture!\n" );
+		success = false;
+	}
+	else
+	{
+		//Set sprites
+		for( int i = 0; i < BUTTON_SPRITE_TOTAL; ++i )
+		{
+			gSpriteClips[ i ].x = 0;
+			gSpriteClips[ i ].y = i * BUTTON_HEIGHT;
+			gSpriteClips[ i ].w = BUTTON_WIDTH;
+			gSpriteClips[ i ].h = BUTTON_HEIGHT;
+		}
+
+
+		//Dibujar botones en sus posiciones
+		gButtons[ 0 ].setPosition( 0, 0 );
+		gButtons[ 1 ].setPosition( 201, 0 );
+		gButtons[ 2 ].setPosition( 402, 0 );
+
+		gButtons[ 3 ].setPosition( 0 , 201);
+		gButtons[ 4 ].setPosition( 201 , 201);
+		gButtons[ 5 ].setPosition( 402, 201);
+
+		gButtons[ 6 ].setPosition( 0, 402 );
+		gButtons[ 7 ].setPosition( 201, 402);
+		gButtons[ 8 ].setPosition( 402, 402 );
 	}
 
 	return success;
@@ -348,27 +593,42 @@ int main( int argc, char* args[] )
 					{
 						quit = true;
 					}
+
+					//Manejar Eventos de boton
+                    for( int i = 0; i < TOTAL_BUTTONS; ++i )
+                    {
+                        gButtons[ i ].handleEvent( &e );
+                    }
 				}
 
 				//Limpiar pantalla
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
-                //Dibujar tablero
 
 
-                //Draw blue horizontal line
+                //Tablero
+                //Color de Lineas
 				SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );
-				SDL_RenderDrawLine( gRenderer,0, SCREEN_HEIGHT / 3, SCREEN_WIDTH, SCREEN_HEIGHT / 3 );
-				SDL_RenderDrawLine( gRenderer,0, SCREEN_HEIGHT / 3*2, SCREEN_WIDTH, SCREEN_HEIGHT / 3*2 );
+				//Dibujar Tablero
+				SDL_RenderDrawLine( gRenderer,0, 200, SCREEN_WIDTH, 200);
+				SDL_RenderDrawLine( gRenderer,0, 400, SCREEN_WIDTH, 400 );
+				SDL_RenderDrawLine( gRenderer,200, 0 , 200 ,SCREEN_HEIGHT);
+				SDL_RenderDrawLine( gRenderer,400, 0 , 400,SCREEN_HEIGHT);
 
-				SDL_RenderDrawLine( gRenderer,SCREEN_WIDTH /3,   0 , SCREEN_WIDTH/3 ,SCREEN_HEIGHT);
-				SDL_RenderDrawLine( gRenderer,SCREEN_WIDTH /3*2, 0 , SCREEN_WIDTH/3*2 ,SCREEN_HEIGHT);
+
+				//Dibujar Botones
+				for( int i = 0; i < TOTAL_BUTTONS; ++i )
+                {
+                    gButtons[ i ].render();
+                }
 
 
 				//Render texture to screen
 				//SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
 
-				//Update screen
+
+
+				//Actualizar pantalla
 				SDL_RenderPresent( gRenderer );
 			}
 		}
